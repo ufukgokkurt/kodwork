3229b63e3b885014b49fabf400337cb411c8e175
Ufuk GÖKKURT <ufuk.gokkurt@gmail.com>
2025-07-26 18:49:43 +0300

    navigation ayarlandı

--- App.tsx
+++ App.tsx
@@ -5,22 +5,72 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+  SafeAreaView,
+} from 'react-native';
+
+function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Text>Home Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+function StackNavigator() {
+  return (
+   
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+  
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Stack" component={StackNavigator} />
+        {/* Diğer Drawer ekranlarını buraya ekleyebilirsiniz */}
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  container2: {
+    backgroundColor: 'yellow',
     flex: 1,
   },
 });
